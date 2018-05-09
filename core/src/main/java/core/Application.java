eb705193635b1f4b269c97b6a21b138237ed413e
Alexander Berry <berryalex123@gmail.com>
2018-05-09 14:02:48 -0700

    Initial commit

---
+++ core/src/main/java/core/Application.java
@@ -0,0 +1,7 @@
+package core;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Application extends ApplicationAdapter {
+}
